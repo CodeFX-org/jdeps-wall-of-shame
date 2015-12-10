dc3a61a55b138fbae0a520470f5aaa3c19e01c05
nipa <nipa@codefx.org>
2015-12-10 21:13:57 +0100

    Use JDeps connection to analyze downloaded artifact

--- src/main/java/org/codefx/jwos/Main.java
+++ src/main/java/org/codefx/jwos/Main.java
@@ -1,14 +1,18 @@
 package org.codefx.jwos;
 
+import com.google.common.collect.ImmutableSet;
 import org.codefx.jwos.MavenCentral.MavenArtifact;
+import org.codefx.jwos.jdeps.JDeps;
+import org.codefx.jwos.jdeps.dependency.Violation;
 import org.eclipse.aether.artifact.DefaultArtifact;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
 		MavenArtifact artifact = new MavenCentral()
-				.downloadMavenArtifact(new DefaultArtifact("org.apache.maven:maven-profile:2.2.1"));
-		System.out.println(artifact);
+				.downloadMavenArtifact(new DefaultArtifact("com.facebook.jcommon:memory:0.1.21"));
+		ImmutableSet<Violation> violations = new JDeps().analyze(artifact.path());
+		System.out.println(violations);
 	}
 
 }
---
+++ src/main/java/org/codefx/jwos/jdeps/JDeps.java
@@ -0,0 +1,38 @@
+package org.codefx.jwos.jdeps;
+
+import com.google.common.collect.ImmutableSet;
+import org.codefx.jwos.jdeps.dependency.Violation;
+import org.codefx.jwos.jdeps.exec.JdkInternalsExecutor;
+import org.codefx.jwos.jdeps.parse.ViolationParser;
+import org.codefx.jwos.jdeps.search.ComposedJDepsSearch;
+import org.codehaus.plexus.util.cli.CommandLineException;
+
+import java.nio.file.Path;
+
+public class JDeps {
+
+	private final Path jdeps;
+
+	public JDeps(Path jdeps) {
+		this.jdeps = jdeps;
+	}
+
+	public JDeps() {
+		this(findJDeps());
+	}
+
+	private static Path findJDeps() {
+		return new ComposedJDepsSearch()
+				.search()
+				.orElseThrow(() -> new IllegalStateException("Could not find JDeps executable."));
+	}
+
+	public ImmutableSet<Violation> analyze(Path artifact) throws CommandLineException {
+		ImmutableSet.Builder<Violation> violations = ImmutableSet.builder();
+		ViolationParser violationParser = new ViolationParser(violations::add);
+		new JdkInternalsExecutor(jdeps, artifact, violationParser::parseLine).execute();
+		violationParser.finish();
+		return violations.build();
+	}
+
+}
